be7532bd480e4d125ba3780fce13962fc30a1240
Luiz Felipe Warmling Amadeu <85834483+Amad3eu@users.noreply.github.com>
2022-04-27 19:57:41 -0300

    Update app.js

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -4,7 +4,7 @@ const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 
-for (let i = 1; i <= 151; i++) {
+for (let i = 1; i <= 451; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
     const label = document.createElement('span');
